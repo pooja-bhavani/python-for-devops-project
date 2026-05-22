dd365c8fc00759a171d9f1f6ccc127de4704f5e4
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2026-05-23 00:02:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from app.api import app  # Importing the app from the app folder and api.py file
-import uvicorn  # Importing uvicorn to run the FastAPI application
+from app.api import app  
+import uvicorn  
 
 if __name__ == "__main__":
-    # this uvicorn will create ASGI. ASGI stands for Asynchronous Server Gateway Interface, which is a specification for building asynchronous web servers in Python.
     uvicorn.run(
-        "app.api:app", # means to run the app kept inside the app folder and api.py file
-        host="0.0.0.0", # if i want the internal team to use this app. we expose it's ip address to
-        port=8000, # i want to run this app on port 8000 note don't use 3306 because it's used by mysql database or the big app ports 
-        reload=True # this will automatically reload the server when we make changes to the code, which is very useful during development.
-
+        "app.api:app", 
+        host="0.0.0.0", 
+        port=8000,  
+        reload=True 
     )
